3558026eeba4d67a3c48632cd8a006cf3be5e273
JenMora <jeniffermourah73@gmail.com>
2023-04-20 11:05:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *Description: main- prints Programming is like building a multilingual puzzle
+ *Return0 : if success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
