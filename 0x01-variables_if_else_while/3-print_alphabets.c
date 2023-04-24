4ecc15462ad3e2b065efb4a036909247068b6341
JenMora <jeniffermourah73@gmail.com>
2023-04-24 09:09:30 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description- Prints a random positive or negative number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
