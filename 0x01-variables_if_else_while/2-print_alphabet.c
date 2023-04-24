a06f9489882fddae1bc5739431d713a4bdb361af
JenMora <jeniffermourah73@gmail.com>
2023-04-24 08:48:37 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description- Prints a random positive or negative number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char c;
+for (c < 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
