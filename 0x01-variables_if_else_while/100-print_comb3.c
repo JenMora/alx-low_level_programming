33bdf9327ba61989b2db43f3b59727e831fb7422
JenMora <jeniffermourah73@gmail.com>
2023-04-24 12:07:14 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description- Prints a random positive or negative number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int a, b;
+
+for (a = '0'; b < '9'; a++)
+{
+for (b = 1 + a; b <= '9'; b++)
+{
+if (b != a)
+{
+putchar(a);
+putchar(b);
+if (a == '8' && b <= '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+
+}
