6f293d0d17628f05a8596f13eefd8bafd2d5eba1
JenMora <jeniffermourah73@gmail.com>
2023-04-24 09:45:20 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description- Prints a random positive or negative number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+printf("%d", a);
+printf("\n");
+return (0);
+}
