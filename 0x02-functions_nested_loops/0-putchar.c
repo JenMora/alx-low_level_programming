012c2a9d8d86ee046cb640fb82ece3396e601333
JenMora <jeniffermourah73@gmail.com>
2023-04-25 02:58:32 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main- Entry point
+ * Description- Prints a random positive or negative number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int putchar(_putchar);
+
+	printf("_putchar\n");
+	return (0);
+}
