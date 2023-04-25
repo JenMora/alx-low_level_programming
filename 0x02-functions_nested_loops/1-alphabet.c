1d91dcabc894435f48a120f9e87a128bfe665fc7
JenMora <jeniffermourah73@gmail.com>
2023-04-25 05:12:46 -0700

    1-alphabet printed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet -print alphabet to stdout
+ *
+ * Return:void
+ */
+void print_alphabet(void)
+{
+char t;
+
+for (t = 'a'; t <= 'z'; t++)
+_putchar(t);
+_putchar('\n');
+}
