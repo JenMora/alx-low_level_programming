4343c24ad4cf655b420de683d265331d5411b4e5
JenMora <jeniffermourah73@gmail.com>
2023-04-27 13:14:07 +0000

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*_mul - prints multiplied integers
+*mul - prints multiplied integers
 *@a: character to be multiplied
 *@b: charecter to be multiplied
 *Return:0 or 1
 */
 int mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
