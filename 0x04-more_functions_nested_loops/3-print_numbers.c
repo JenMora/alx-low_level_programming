a46bfbfbf8d55c9dd028dfbec3e14858d261be45
JenMora <jeniffermourah73@gmail.com>
2023-04-27 13:58:48 +0000

    3-print_number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints numbers from 0 to 9 followed by a new line
+ *@c:the character for the task
+ *Return:0
+ */
+void print_numbers(void)
+{
+char c = 48;
+	
+while (c <= 57)
+{
+_putchar('c');
+c++;
+}
+_putchar('\n');
+}
