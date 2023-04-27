710a59d07a81685e76f8d8802e66fae62c5008b6
JenMora <jeniffermourah73@gmail.com>
2023-04-27 12:22:28 +0000

    1-isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-*_isupper - check for upper case letter
-*@c: character to check the letter case
+*_isdigit - checks for a digit (0 through 9)
+*@c: character to check the digit
 *Return:0 or 1
 */
 int _isdigit(int c)
