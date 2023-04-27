e9a6e521f832ebf45eed81a84dc2592831a9e9ba
JenMora <jeniffermourah73@gmail.com>
2023-04-27 12:18:23 +0000

    1-isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *_isupper - check for upper case letter
- *@c: character to check the letter case
- *Return:0 or 1
- */
+*_isupper - check for upper case letter
+*@c: character to check the letter case
+*Return:0 or 1
+*/
 int _isdigit(int c)
 {
 return (c >= 48 && c <= 57);
