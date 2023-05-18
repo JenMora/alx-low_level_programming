a7240ef1f92c438ea6713434328422167e334cf7
JenMora <jeniffermourah73@gmail.com>
2023-05-18 13:51:25 +0000

    the commit message dependent on the subsequent file name

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_memset - copy char
+ *@s: string
+ *@m: character input
+ *@n: number of bytes
+ *Return: string
+ */
+char *_memset(char *s, char m, unsigned int n)
+{
+	    unsigned int a;
+
+		for (a = 0; a < n; a++)
+{
+			s[a] = m;
+}
+	return (s);
+}
+
+/**
+ *_calloc - allocates memory for an array using malloc
+ *@nmemb: number of elements
+ *@size: bytes
+ *Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *pointer;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	pointer = malloc(nmemb * size);
+
+	if (pointer == NULL)
+
+	return (NULL);
+
+	_memset(pointer, 0,(nmemb * size));
+
+	return (pointer);
+}
