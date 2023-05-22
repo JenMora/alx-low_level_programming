ef1628cc147f3950b60c287cbc0eb23d914c2caf
JenMora <jeniffermourah73@gmail.com>
2023-05-22 16:36:30 +0000

    the commit message dependent on the subsequent file name

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog- a new type struct for dog
+ * @name: the name of the dog
+ * @age: the age of teh dog
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif /*DOG_H*/
