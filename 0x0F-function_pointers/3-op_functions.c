449dfa6a253e7a856991f6cd33eb8e0a80e61b78
JenMora <jeniffermourah73@gmail.com>
2023-05-24 18:26:53 +0000

    the commit message dependent on the subsequent file name

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,59 +1,59 @@
 #include "3-calc.h"
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
- *op_add -a function that calculates the sum of two integers
- *@a: the first integer
- *@b:the second integer
- *Return: the sum of a and b
+ *op_add - Returns the sum of two numbers.
+ *@a: The first number.
+ *@b: The second number.
+ *Return: The sum of a and b.
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- *op_mul -a function that calculates the product of two integers
- *@a: the first integer
- *@b:the second integer
- *Return: the product of a and b
- *
- *
- */
-
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- *op_sub -a function that calculates the difference of two integers
- *@a: the first integer
- *@b:the second integer
- *Return: the difference of a and b
+ *op_sub - Returns the difference of two numbers.
+ *@a: The first number.
+ *@b: The second number.
+ *Return: The difference of a and b.
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- *op_div -a function that calculates the division of two integers
- *@a: the first integer
- *@b:the second integer
- *Return: the div of a and b
+ *op_mul - Returns the product of two numbers.
+ *@a: The first number.
+ *@b: The second number.
+ *Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - Returns the division of two numbers.
+ *@a: The first number.
+ *@b: The second number.
+ *Return: The quotient of a and b.
  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
 /**
- *op_mod -a function that calculates the sum of two integers
- *@a: the first integer
- *@b:the second integer
- *Return: the modulus of a and b
+ *op_mod - Returns the remainder of the division of two numbers.
+ *@a: The first number.
+ *@b: The second number.
+ *Return: The remainder of the division of a by b.
  */
 int op_mod(int a, int b)
 {
 	return (a % b);
 }
+
