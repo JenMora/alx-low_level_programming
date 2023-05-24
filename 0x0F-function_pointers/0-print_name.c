0867c1bffd50682968e7a7f3d0cd4fd2e7ef9df6
JenMora <jeniffermourah73@gmail.com>
2023-05-24 09:13:42 +0000

    the commit message dependent on the subsequent file name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - a function that prints a name
+ * @name: teh name to be printed
+ * @f: the function pointer
+ * @char: the string for the name
+ * Return: 0 if success
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
