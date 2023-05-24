848a442af6656be1da720d00c8cdd7cf8037f25d
JenMora <jeniffermourah73@gmail.com>
2023-05-24 12:58:00 +0000

    the commit message dependent on the subsequent file name

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point of the program
+ * @argc: the argument count
+ * @argv:arguments array
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char s;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+	
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	s = *argv[2];
+
+	if ((s == '%' || s == '/') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = func ( arg1, arg2);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+
+/**
+ *op_add -a function that calculates the sum of two integers
+ *@a: the first integer
+ *@b:the second integer
+ *Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_mul -a function that calculates the product of two integers
+ *@a: the first integer
+ *@b:the second integer
+ *Return: the product of a and b
+ *
+ *
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_sub -a function that calculates the difference of two integers
+ *@a: the first integer
+ *@b:the second integer
+ *Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_div -a function that calculates the division of two integers
+ *@a: the first integer
+ *@b:the second integer
+ *Return: the div of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod -a function that calculates the sum of two integers
+ *@a: the first integer
+ *@b:the second integer
+ *Return: the modulus of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
