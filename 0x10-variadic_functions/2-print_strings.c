e5f5842edfbd97a2ac5bc88a693afdcc2dd17cd9
JenMora <jeniffermourah73@gmail.com>
2023-05-25 14:21:59 +0000

    the commit message dependent on the subsequent file name

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - a function that prints strings
+ * @separator: teh string to be printed  between strings
+ * @n: the number of strings passed to teh function
+ * @...: the strings to be printed
+ * Return:0
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list printedstrings;
+
+	unsigned int i;
+
+	char *str;
+
+	va_start(printedstrings, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(printedstrings, char *);
+		if (str)
+			printf("%s", str);
+		else
+
+		printf("nil");
+
+		if (i < n - 1)
+		if (separator)
+
+		printf("%s", separator);
+	}
+		printf("\n");
+		va_end(printedstrings);
+}
