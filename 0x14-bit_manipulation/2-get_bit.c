0ac585d66e0e9a69152f3245919d064449da863c
JenMora <jeniffermourah73@gmail.com>
2023-06-08 13:33:21 +0000

    the commit message dependent on the subsequent file name

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,6 +5,7 @@
 /**
  * get_bit - a function that returns the value of a bit at a given index
  * @index:the index, starting from 0 of the bit you want to get
+ * @n:unsigned long integer
  * Return:the value of the bit at index index or -1 if an error occured
  */
 
@@ -17,8 +18,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	return (-1);
 	}
 /*if error occurs i.e index out of range*/
-	return (n & mask) ? 1 : 0;
+	return ((n & mask) ? 1 : 0);
 /*return the result of the bit at a given index*/
-/*the ? stands for a conditional operator,
- *after comparing n and mask using & bitwise operator*/
 }
