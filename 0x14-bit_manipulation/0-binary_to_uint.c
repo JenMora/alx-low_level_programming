6c800f026fcf925dc76d735f1a9dec9db4b6f8ec
JenMora <jeniffermourah73@gmail.com>
2023-06-08 12:28:07 +0000

    the commit message dependent on the subsequent file name

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /*
- * binary_to_uint-converts a binary number to an unsigned int
+ * binary_to_uint - a function that converts a binary number to an unsigned int
  * @b:points to a string of 0 and 1 characters
  * Return:the converted number if success, otherwise 0 if one or more
  * characters exist in the string b that is not 0 or 1 or if b is NULL
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* print_binary -  prints the binary representation of a number
+* @n: the unsigned long integer
+* Return: 0
+*/
+
+void print_binary(unsigned long int n)
+{
+unsigned long int integer = n;
+int bits = 0;
+int i;
+/*for bit iteration*/
+
+while (integer > 0)
+{
+integer >>= 1;
+/*right shift by one bit*/
+bits++;
+/*increment the bit count*/
+}
+/*print the binary of the integer*/
+for (i = bits - 1; i >= 0; i--)
+{
+unsigned long int mask = 1UL << i;
+/*creating a bit mask by left shifting */
+if (n & mask)
+/*this checks whether the bit is set*/
+printf("0");
+else
+printf("1");	
+}
+}
