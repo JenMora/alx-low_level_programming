a29462c04e8b5cec685a60ef427393248902221e
JenMora <jeniffermourah73@gmail.com>
2023-06-08 14:07:53 +0000

    the commit message dependent on the subsequent file name

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,11 +10,9 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int mask = ~(1UL << index);
-
-*n = *n & mask;
+*n &= ~(1 << index);
 return (1);
-if (index >= (sizeof(unsigned long int) * 8))
+if (index >= (sizeof(n) * 8))
 {
 return (-1);
 }
