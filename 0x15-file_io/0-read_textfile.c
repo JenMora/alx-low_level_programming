a74c6c70de2596a5a9794c1d1d50f24622ec1c16
JenMora <jeniffermourah73@gmail.com>
2023-06-12 15:10:44 +0000

    the commit message dependent on the subsequent file name

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,6 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t r;
 	ssize_t w;
 	char *letter_file;
+
 	if (filename == 0)
 	{
 	return (0);
@@ -25,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	o = open(filename, O_RDONLY);
-	r = read(0, letter_file, letters);
+	r = read(o, letter_file, letters);
 	w = write(STDOUT_FILENO, letter_file, r);
 
 	if (o == -1 || w == -1 || r == -1 || w != r)
