a1501dc09a5e4cd895ff6a3ef78f14ff4c31049e
JenMora <jeniffermourah73@gmail.com>
2023-06-12 15:39:43 +0000

    the commit message dependent on the subsequent file name

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,11 +15,11 @@ int create_file(const char *filename, char *text_content)
 	int j = 0;
 	int length = 0;
 
-	if (filename == 0)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
-	if (text_content != 0)
+	if (text_content != NULL)
 	{
 		while (text_content[length])
 			length++;
@@ -30,6 +30,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (i == -1 || j == -1)
 	{
+	return (-1);
 	close(i);
 	}
 	return (1);
