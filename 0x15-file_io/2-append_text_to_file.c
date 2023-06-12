2a05c5f1199c4b7bfc5ae5111cb4f29682474e28
JenMora <jeniffermourah73@gmail.com>
2023-06-12 18:25:18 +0000

    the commit message dependent on the subsequent file name

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,12 +15,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	int j = 0;
 	int length = 0;
 
-	if (filename == 0)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	if (text_content == 0)
+	if (text_content == NULL)
 	{
 		while (text_content[length])
 			length++;
