ddd4dcacce81feaf4ec34722f7b6b1286b786ee3
JenMora <jeniffermourah73@gmail.com>
2023-08-16 11:10:25 -0700

    This is a correction of the function that deletes a hashtable and frees memory to avoid memory leaks

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,30 +1,39 @@
 #include "hash_tables.h"
 
+
+
 /**
- * hash_table_delete - This i a function that delete a hash table
- * @ht: the hash table
+ * free_node - This is a user defined function that frees the node
+ * @node: The node pointer to be freed
+ * Return: 0
+ */
+void free_node(hash_node_t *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+/**
+ * hash_table_delete - This is a function to deletes a hash table
+ * @ht: This is the hash table to be deleted
+ * Return: void
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *tmp, *node;
-	unsigned long int integer, size;
+	hash_node_t *node, *tmp;
+	unsigned long int integer = 0;
 
-	if (ht->array == NULL)
+	if (ht == NULL || ht->array == NULL)
 	{
-		free(ht);
 		return;
 	}
-	size = ht->size;
-
-	for (; integer < size; integer++)
+	for (; integer < ht->size; integer++)
 	{
 		node = ht->array[integer];
 		while (node)
 		{
 			tmp = node->next;
-			free(node->key);
-			free(node->value);
-			free(node);
+			free_node(node);
 			node = tmp;
 		}
 	}
